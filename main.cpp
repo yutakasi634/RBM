022e7191246b16fae093c91e7c0e107be31e5255
yutakasi634 <p7dea87c7e4n7j@gmail.com>
2017-01-11 16:18:11 +0900

    add template argument for connection matrix

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <fstream>
 #include <random>
 
-template<typename T>
-vector<vector<T>>& getDataVector(std::string filename){
+template<typename potentialType>
+vector<vector<potentialType>>& getDataVector(std::string filename){
   std::ifstream reading_file;
   reading_file.open(filename,std::ios::in);
   std::string reading_line_buffer;
@@ -12,34 +12,34 @@ vector<vector<T>>& getDataVector(std::string filename){
   char comma;
 
   while(!reading_file.eof()){
-    std::vector<T> temp_data;
+    std::vector<potentialType> temp_data;
     getline(reading_file,reading_line_buffer);
     std::istringstream is(reading_line_buffer);
     while(!is.eof()){
       is >> num >> comma;
       temp_data.push_back(num);
     }
-    std::vector<vector<T>> data;
+    std::vector<vector<potentialType>> data;
     data.push_back(temp_data);
   }
   return data;
 }
 
-template<typename T>
+template<typename potentialType,typename connectionType>
 class RBM{
 protected:
-  std::vector<std::vector<T>> potential;
-  std::vector<std::vector<double>> connectionMatrix;
+  std::vector<std::vector<potentialType>> potential;
+  std::vector<std::vector<connectionType>> connectionMatrix;
 public:
-  RBM(std::vector<T> initialValues);
-  void setVisiblePotentials(std::vector<T>& values);
-  void setConnectionMatrix(std::vector<std::vector<double>>& weightMatrix);
+  RBM(std::vector<potentialType> initialValues);
+  void setVisiblePotentials(std::vector<potentialType>& values);
+  void setConnectionMatrix(std::vector<std::vector<connectionType>>& weightMatrix);
 };
 
-template<typename T>
-RBM<T>::RBM(std::vector<T> initialValues){
+template<typename potentialType, typename connectionType>
+RBM<potentialType,connectionType>::RBM(std::vector<potentialType>& initialValues){
   potential.push_back(initialValue);
-  std::vector<T> hiddenLayer(initialValue.size());
+  std::vector<potentialType> hiddenLayer(initialValue.size());
   potential.push_back(hiddenLayer);
   connectionMatrix.resize(initialValue.size());
   for(std::size_t i = 0; i < initialValue.size(); ++i){
@@ -50,13 +50,13 @@ RBM<T>::RBM(std::vector<T> initialValues){
   }
 }
 
-template<typename T>
-void RBM<T>::setVisiblePotentials(std::vector<T>& values){
+template<typename potentialType, typename connectionType>
+void RBM<potentialType,connectionType>::setVisiblePotentials(std::vector<potentialType>& values){
   potential.at(0) = values;
 }
 
-template<typename T>
-void RBM<T>::setConnectionMatrix(std::vector<std::vector<double>>& weightMatrix){
+template<typename potentialType, typename connectionType>
+void RBM<potentialType>::setConnectionMatrix(std::vector<std::vector<connectionType>>& weightMatrix){
   connectionMatrix = weightMatrix;
 }
 
