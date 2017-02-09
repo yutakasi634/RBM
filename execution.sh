55500caa2dfa6271ca5d6642ac106bb4c232b1db
yutaka <p7dea87c7e4n7j@gmail.com>
2017-02-09 19:03:44 +0900

    rename main.cpp to brbmlearning.cpp

--- execution.sh
+++ execution.sh
@@ -5,7 +5,7 @@ read LEARNINGSTEP
 echo "totalOutputStep is"
 read OUTPUTSTEP
  python RBMprogram/translateToOneliner.py $1input.dat \
-&& ./build/rbmlerning $1 $LEARNINGSTEP $OUTPUTSTEP \
+&& ./build/rbmlearning $1 $LEARNINGSTEP $OUTPUTSTEP \
 && python3 plot/plotmatrix.py $1answer.dat
 python3 plot/plotmatrix.py $1input.dat &
 python3 plot/plotconnection.py $1connection.dat &
