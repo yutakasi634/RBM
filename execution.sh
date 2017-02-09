8ca2aa5005fb06b0df7abf14d026d9199d83929e
yutaka <p7dea87c7e4n7j@gmail.com>
2017-02-09 18:21:02 +0900

    automate display

--- execution.sh
+++ execution.sh
@@ -3,13 +3,15 @@
 echo "totalLearningStep is" 
 read LEARNINGSTEP 
 echo "totalOutputStep is"
-read OUTPUTSTEP \
-&& python RBMprogram/translateToOneliner.py $1input.dat \
+read OUTPUTSTEP
+ python RBMprogram/translateToOneliner.py $1input.dat \
 && ./build/rbmlerning $1 $LEARNINGSTEP $OUTPUTSTEP \
 && python3 plot/plotmatrix.py $1answer.dat
-python3 plot/plotmatrix.py $1input.dat
-python3 plot/plotconnection.py $1connection.dat
-python3 plot/plot.py $1differentiation.dat \
-&& display $1input.png
-display $1connection.png
-display $1differentiation.png
+python3 plot/plotmatrix.py $1input.dat &
+python3 plot/plotconnection.py $1connection.dat &
+python3 plot/plot.py $1differentiation.dat &
+wait
+display $1input.png &
+display $1connection.png &
+display $1differentiation.png &
+display $1answer.png &
