90f045a0e3a5488b33f1adc06f1f804ee61c6abe
yutaka <p7dea87c7e4n7j@gmail.com>
2017-02-12 16:51:21 +0900

    add line for debug

--- RBMprogram/getDataVector.hpp
+++ RBMprogram/getDataVector.hpp
@@ -20,20 +20,16 @@ std::vector<std::vector<potentialType>> getDataVector(std::string filename){
 
   std::vector<std::vector<potentialType>> data;
   while(getline(reading_file,reading_line_buffer)){
-    //    std::cout << "reading_line_buffer " << reading_line_buffer << std::endl;
     std::istringstream is(reading_line_buffer);
     std::vector<potentialType> temp_data;
     std::string token;
-    //std::cout << "step1" << std::endl;
     while(getline(is,token,',')){
       std::istringstream numtoken(token);
       numtoken >> num;
-      //std::cout << num << std::endl;
       temp_data.push_back(num);
-      //std::cout << "step2" << std::endl;
     }
     data.push_back(temp_data);
-    //    reading_file.peek();
+    reading_file.peek();
   }
   reading_file.close();
   return data;
